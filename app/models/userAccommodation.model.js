5defab9f39905b96730cc725c6c6851308bdaffb
JaxenM <jxnmcray@gmail.com>
2023-11-05 16:13:42 -0600

    Implemented way for students to request accommodations, creating them in the db

--- app/models/userAccommodation.model.js
+++ app/models/userAccommodation.model.js
@@ -1,16 +1,34 @@
 module.exports = (sequelize, Sequelize) => {
-    const UserAccommodation = sequelize.define("userAccommodation", {
-        id: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-      permission: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-      },
-    }, {
-      timestamps: false,
-    });
-    return UserAccommodation;
-  }
+  const UserAccommodation = sequelize.define("userAccommodation", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    permission: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    accommodationCategoryId: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: true // or false if you want to make it required
+    },
+    // Add other fields such as semesterId if necessary
+    // semesterId: {
+    //   type: Sequelize.INTEGER,
+    //   allowNull: true, // or false if it is required
+    // }
+  }, {
+    timestamps: false
+  });
+
+  return UserAccommodation;
+};
