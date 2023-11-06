ace05dbc0ec54bfb0b4198a7f993d151943082dc
Zfike <97480418+Zfike@users.noreply.github.com>
2023-11-06 13:48:28 -0600

    Revert "Backend email service zf"

--- app/controllers/section.controller.js
+++ app/controllers/section.controller.js
@@ -4,7 +4,7 @@ const Op = db.Sequelize.Op;
 // Create and Save a new Section
 exports.create = (req, res) => {
   // Validate request
-  if (!req.body.id) {
+  if (!req.body.title) {
     res.status(400).send({
       message: "Content can not be empty!",
     });
